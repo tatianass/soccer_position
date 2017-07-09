9696ea51adc9c8014afe838907dc2486e5c92bbf
tatianass <tatiana.silva@ccc.ufcg.edu.br>
2017-07-09 20:59:46 -0300

    analysing statically significance of variables

--- R/analysis.R
+++ R/analysis.R
@@ -59,3 +59,42 @@ data[data$preferred_foot == "right",]$preferred_foot = 1
 data <- left_join(field_positions, data, by = c("position" = "position")) %>%
   select(-position)
 colnames(data)[colnames(data) == 'id'] <- 'id_position'
+
+anova_fun <- function(analysis, type){
+  
+  #anova
+  #h0: they are equal, h1: they are not (p<0.05)
+  anova <- aov(analysis)
+  
+  file_name <- " anova sumary.txt"
+  file_name <- paste(type, file_name)
+  s<-summary(anova)
+  capture.output(s, file = file_name)
+  
+  name <- " plot witch 1.png"
+  name <- paste(type, name)
+  png(filename=name)
+  #Caso queira visualizar o gráfico
+  plot(analysis, which = 1)
+  dev.off()
+  
+  name <- " plot witch 2.png"
+  name <- paste(type, name)
+  png(filename=name)
+  #can se that there's a positive skewness in the data
+  plot(analysis, which = 2)
+  dev.off()
+  
+  sresids <- rstandard(analysis)
+  
+  name <- " histogram.png"
+  name <- paste(type, name)
+  png(filename=name)
+  hist(sresids)
+  dev.off()
+}
+
+#fitting data to anova
+aov_type <- lm(id_position~.,data=data)
+
+anova_fun(aov_type, "analyse/position")
